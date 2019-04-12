bf1a2b2cdb615715bf830ae27e02046ed0ef72f4
Haibiao XIAO <haibiao.xiao@formal-tech.com>
2019-04-12 16:01:17 +0800

    Update tms570_rti.c

--- hw/timer/tms570_rti.c
+++ hw/timer/tms570_rti.c
@@ -22,6 +22,9 @@ typedef struct RTIState {
     SysBusDevice parent_obj;
 
     MemoryRegion iomem;
+    ptimer_state *timer[3];
+
+    uint32_t overflow_flag[2];
 
     uint32_t global_ctrl;               /* RTIGCTRL */
     uint32_t timebase_ctrl;             /* RTITBCTRL */
@@ -29,8 +32,7 @@ typedef struct RTIState {
     uint32_t compare_ctrl;              /* RTICOMPCTRL */
 
     uint32_t free_running_counter[2];   /* RTIFRC0~1 >>5 */
-    uint32_t up_counter[2];             
-    uint32_t up_counter_reg[2]          /* RTIUC0~1 >>5 */
+    uint32_t up_counter[2];             /* RTIUC0~1 >>5 */
     uint32_t compare_up_counter[2];     /* RTICPUC0~1 >>5 */
     uint32_t capture_free_counter[2];   /* RTICAFRC0~1 >>5 */
     uint32_t capture_up_counter[2];     /* RTICAUC0~1 >>5 */
@@ -51,13 +53,11 @@ typedef struct RTIState {
     uint32_t rti_dw_down_counter;       /* RTIDWDCNTR */
     uint32_t dww_reaction_ctrl;         /* RTIWWDRXNCTRL */
     uint32_t dww_size_ctrl;             /* RTIWWDSIZECTRL */
+
+    uint32_t rti_reset_flag;
     
-    qemu_irq irq_compare0;
-    qemu_irq irq_compare1;
-    qemu_irq irq_compare2;
-    qemu_irq irq_compare3;
-    qemu_irq irq_overflow0;
-    qemu_irq irq_overflow1;
+    qemu_irq irq_compare[4];
+    qemu_irq irq_overflow[2];
     qemu_irq irq_timebase;
 } RTIState;
 
@@ -68,6 +68,47 @@ static void dw_reset(RTIState *s)
     s->rti_wd_key = 0xa35c;
     s->rti_dw_down_counter = 0xffff;
     s->dww_reaction_ctrl = 0x5;
+}
+
+static void rti_update(RTIState *s)
+{
+    rti_update_irq(s);
+}
+
+static void rti_update_irq(RTIState *s)
+{
+    /* enabled and pending interrupt */
+    uint32_t en_pend_irq = s->int_ctrl & s->int_flag & 0x0007000f;
+
+    for (size_t i = 0; i < 4; i++)
+    {
+        qemu_set_irq(irq_compare[i], en_pend_irq & (0x1 << i));
+    }
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        qemu_set_irq(irq_overflow[i], en_pend_irq & (0x1 << (i + 17)));
+    }
+
+    qemu_set_irq(irq_timebase, en_pend_irq & (0x1 << 16));
+    
+}
+
+static void rti_update_compare(RTIState *s, int counter_num)
+{
+    for (size_t i = 0; i < 4; i++)
+    {
+        if ((s->compare_ctrl << (i*4)) == counter_num)
+        {
+            if (s->free_running_counter[counter_num] == s->compare[i])
+            { //compare int
+                s->compare[i] += s->update_compare[i]; 
+                s->int_flag |= 0x1 << i;
+            }
+        }
+    }
+    
+    rti_update_irq(s);
 }
 
 static uint64_t rti_read(void *opaque, hwaddr offset,
@@ -81,14 +122,13 @@ static uint64_t rti_read(void *opaque, hwaddr offset,
 
         if (index & 0x001)
         { /* RTIUDCP0~3 */
-            return update_compare[(index >> 1)];
+            return s->update_compare[(index >> 1)];
         } else 
         { /* RTICOMP0~3 */
-            return compare[(index >> 1)];
+            return s->compare[(index >> 1)];
         }
         
     }
-    
 
     switch (offset)
     {
@@ -102,10 +142,10 @@ static uint64_t rti_read(void *opaque, hwaddr offset,
             return s->compare_ctrl;
 
         case 0x10: /* RTIFRC0 */
-            s->up_counter_reg[0] = s->up_counter[0]
+            s->up_counter[0] = ptimer_get_count(s->timer[0]);
             return s->free_running_counter[0];
         case 0x14: /* RTIUC0 */
-            return s->up_counter_reg[0];
+            return s->up_counter[0];
         case 0x18: /* RTICPUC0 */
             return s->compare_up_counter[0];
         case 0x20: /* RTICAFRC0 */
@@ -114,10 +154,10 @@ static uint64_t rti_read(void *opaque, hwaddr offset,
             return s->capture_up_counter[0];
 
         case 0x30: /* RTIFRC1 */
-            s->up_counter_reg[1] = s->up_counter[1]
+            s->up_counter[1] = ptimer_get_count(s->timer[1]);
             return s->free_running_counter[1];
         case 0x34: /* RTIUC1 */
-            return s->up_counter_reg[1];
+            return s->up_counter[1];
         case 0x38: /* RTICPUC1 */
             return s->compare_up_counter[1];
         case 0x40: /* RTICAFRC1 */
@@ -146,7 +186,7 @@ static uint64_t rti_read(void *opaque, hwaddr offset,
         case 0x9c: /* RTIWDKEY */
             return s->rti_wd_key;
         case 0xa0: /* RTIDWDCNTR */
-            return s->rti_dw_down_counter;
+            return s->rti_dw_down_counter = ptimer_get_count(s->timer[2]);
         case 0xa4: /* RTIWWDRXNCTRL */
             return s->dww_reaction_ctrl;
         case 0xa8: /* RTIWWDSIZECTRL */
@@ -169,10 +209,10 @@ static void rti_write(void *opaque, hwaddr offset,
 
         if (index & 0x001)
         { /* RTIUDCP0~3 */
-            update_compare[(index >> 1)] = val;
+            s->update_compare[(index >> 1)] = val;
         } else 
         { /* RTICOMP0~3 */
-            compare[(index >> 1)] = val;
+            s->compare[(index >> 1)] = val;
         }
         
     }
@@ -181,22 +221,41 @@ static void rti_write(void *opaque, hwaddr offset,
     {
         case 0x00: /* RTIGCTRL */
             s->global_ctrl = val;
+            for (size_t i = 0; i < 2; i++)
+            {
+                int bit = 0x1 << i;
+                /* counter0~1 */
+                if ((s->global_ctrl & bit) == bit)
+                { //enable
+                    ptimer_run(s->timer[i], 0);
+                } else
+                { //disable
+                    ptimer_stop(s->timer[i]);
+                }
+            }
+            if ((s->global_ctrl & 0x8000) != 0x8000)
+            {   /* halting debug mode */
+                ptimer_stop(s->timer[0]);
+                ptimer_stop(s->timer[1]);
+            }
             break;
         case 0x04: /* RTITBCTRL */
-            s->timebase_ctrl = val;
+            s->timebase_ctrl = val; //todo
             break;
         case 0x08: /* RTICAPCTRL */
             s->capture_ctrl = val;
             break;
         case 0x0c: /* RTICOMPCTRL */
-            s->compare_ctrl = val;
+            s->compare_ctrl = val & 0x00001111;
             break; 
 
         case 0x10: /* RTIFRC0 */
             s->free_running_counter[0] = val;
+            s->overflow_flag[0] = 0;
             break;
         case 0x14: /* RTIUC0 */
             s->up_counter[0] = val;
+            ptimer_set_count(s->timer[0], s->upcounter[0]);
             break; 
         case 0x18: /* RTICPUC0 */
             if ((s->timebase_ctrl & 0x1) == 0x0)
@@ -211,9 +270,11 @@ static void rti_write(void *opaque, hwaddr offset,
 
         case 0x30: /* RTIFRC1 */
             s->free_running_counter[1] = val;
+            s->overflow_flag[0] = 0;
             break;
         case 0x34: /* RTIUC1 */
             s->up_counter[1] = val;
+            ptimer_set_count(s->timer[1], s->upcounter[1]);
             break; 
         case 0x38: /* RTICPUC1 */
             s->compare_up_counter[1] = val;
@@ -251,12 +312,14 @@ static void rti_write(void *opaque, hwaddr offset,
             if (val == 0xa98559da)
             {
                 s->dw_ctrl = val;
+                ptimer_run(s->timer[2], 1);
             }
             break;
         case 0x94: /* RTIDWPRLD */
             if (s->dw_ctrl == 0x5312aced)
             {
                 s->dw_preload = val;
+                ptimer_set_limit(s->timer[2], s->dw_preload, 1);
             }
             break;
         case 0x98: /* RTIDWSTATUS */
@@ -267,10 +330,10 @@ static void rti_write(void *opaque, hwaddr offset,
             {
                 if (val == 0xa35c)
                 {   /* watchdog is reset */
-                    
+                    dw_reset(s);
                 }else if (val == 0xe51a)
                 {   /* WDKEY is enabled for reset or NMI by next A35Ch */
-
+                    s->rti_reset_flag = 1;
                 }
                 break;
             }
@@ -305,6 +368,8 @@ static void rti_write(void *opaque, hwaddr offset,
         default:
             break;
     }
+
+    rti_update(s);
 }
 
 static void rti_get_cap(void *opaque, int irq, int level)
@@ -334,11 +399,35 @@ static void rti_reset(DeviceState *dev)
 {
     RTIState *s = RTI(dev);
 
-    s->dw_ctrl = 0x5312aced;
-    s->dw_preload = 0xfff;
-    s->rti_wd_key = 0xa35c;
-    s->rti_dw_down_counter = 0xffff;
-    s->dww_reaction_ctrl = 0x5;
+    s->global_ctrl = 0x0;
+    s->timebase_ctrl = 0x0;
+    s->capture_ctrl = 0x0;
+    s->compare_ctrl = 0x0;
+
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        s->free_running_counter[i] = 0;
+        s->up_counter[i] = 0;
+        s->up_counter_reg[i] = 0;
+        s->compare_up_counter[i] = 0;
+        s->capture_free_counter[i] = 0;
+        s->capture_up_counter[i] = 0;
+    }
+
+    for (size_t i = 0; i < 4; i++)
+    {
+        s->compare[i] = 0;
+        s->update_compare = 0;
+    }
+    
+    s->timebase_low = 0;
+    s->timebase_high = 0;
+    s->int_ctrl = 0;
+    s->int_flag = 0;
+
+    dw_reset(s);
+    rti_update(s);
 }
 
 static const MemoryRegionOps rti_ops = {
@@ -347,23 +436,72 @@ static const MemoryRegionOps rti_ops = {
     .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
+static void rti_timer_tick(void *opaque)
+{
+    RTIState *s = (RTIState *)opaque;
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        if (s->up_counter[i] == s->compare_up_counter[i])
+        {
+            s->up_counter[i] = 0;
+            s->free_running_counter[i]++;
+            s->overflow_flag[i] = 1;
+            rti_update_compare(s, i);
+        }
+        
+        if ((s->free_running_counter[i] == 0) && s->overflow_flag[i])
+        {  /* overflow */
+            s->int_flag |= 0x1 << (17 + i); // overflow int
+            s->overflow_flag[i] = 0;
+            rti_update_irq(s);
+        }
+    }
+}
+
+static void rti_watchdog_tick(void *opaque)
+{
+    RTIState *s = (RTIState *)opaque;
+
+    if (ptimer_get_count(s->timer[2]) == 0 && s->rti_reset_flag)
+    {
+        s->rti_reset_flag = 0;
+        rti_reset(s);
+    }
+}
+
 static void rti_init(Object *obj)
 {
     RTIState *s = RTI(obj);
     SysBusDevice *dev = SYS_BUS_DEVICE(obj);
+    QEMUBH *bh[3];
 
     memory_region_init_io(&s->iomem, obj, &rti_ops, s, "tms570-rti", 0x100);
     sysbus_init_mmio(sbd, &s->iomem);
 
+    /* init timer */
+    for (size_t i = 0; i < 2; i++)
+    {
+        bh[i] = qemu_bh_new(rti_timer_tick, s);
+        s->timer[i] = ptimer_init(bh[i]);
+    }
+
+    bh[2] = qemu_bh_new(rti_watchdog_tick, s);
+    s->timer[2] = ptimer_init(bh[2]);
+
     /* CAP event rti0 & rti1 */
     qdev_init_gpio_in(dev, rti_get_cap, 2);
 
-    sysbus_init_irq(dev, &s->irq_compare0);
-    sysbus_init_irq(dev, &s->irq_compare1);
-    sysbus_init_irq(dev, &s->irq_compare2);
-    sysbus_init_irq(dev, &s->irq_compare3);
-    sysbus_init_irq(dev, &s->irq_overflow0);
-    sysbus_init_irq(dev, &s->irq_overflow1);
+    for (size_t i = 0; i < 4; i++)
+    {
+        sysbus_init_irq(dev, &s->irq_compare[i]);
+    }
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        sysbus_init_irq(dev, &s->irq_overflow[i]);
+    }
+
     sysbus_init_irq(dev, &s->irq_timebase);
 }
 
