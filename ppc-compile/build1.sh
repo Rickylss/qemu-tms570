6499874bb47df573fb54bed8e72d1b1200bef24b
qian tang <qian.tang@formal-tech.com>
2019-04-26 16:33:46 +0800

    修正winxp编译脚本

---
+++ ppc-compile/build1.sh
@@ -0,0 +1,12 @@
+if [ $# != 1];then
+	echo "usage erron"
+else
+	PD=`pwd`
+	srcpath=$PD"/../configure"
+	buildpath=$PD"/../../ppc-build/"$1
+	mkdir $1
+	cd $1
+	$srcpath --prefix=$buildpath --target-list=ppc-softmmu --cross-prefix=i686-w64-mingw32- --disable-docs --disable-gnutls --disable-curl --disable-libssh2  --enable-gtk  --enable-sdl --enable-debug --disable-pie
+	make -j3
+
+fi
